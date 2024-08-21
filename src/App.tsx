dd7e2be693b65817637b66fe5ac69f823fb8ad3c
kodegv <celsocharlez1@gmail.com>
2024-08-21 18:33:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,53 @@ import GenreDetail from './pages/GenresDetail/GenreDetail';
 import './App.css' // global styling
 import './styles/themes.css'
 
+
+
 const getInitialTheme = () => {
-    const savedTheme = localStorage.getItem('theme') as 'light' | 'dark';
-    return savedTheme ? savedTheme : 'dark';
-  }
+  const savedTheme = localStorage.getItem('theme') as 'light' | 'dark';
+  return savedTheme ? savedTheme : 'dark';
+};
+
+export default function App() {
+
+  const [theme, setTheme] = useState<'light' | 'dark'>(getInitialTheme);
+
+
+  useEffect(() => {
+    document.body.classList.remove('light-theme', 'dark-theme');
+    document.body.classList.add(theme === 'light' ? 'light-theme' : 'dark-theme');
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+
+  
+
+
+  return (
+  <AudioPlayerProvider> 
+    <Router>
+      <Header 
+        toggleTheme={toggleTheme} 
+        theme={theme}
+      />
+      <div className='body-content'>
+      <ScrollToTop>
+      <Routes>
+              <Route path="/" element={<Shows/>} />
+              <Route path="show/:id" element={<ShowDetail />}>
+                <Route path="season/:seasonId" element={<SeasonDetail />} />
+              </Route>
+              <Route path="/favourites" element={<Favourites />} />
+              <Route path="/genres" element={<Genres />} />
+              <Route path="/genre/:genreSlug" element={<GenreDetail />} />
+            </Routes>
+        </ScrollToTop>
+      </div>
+      <AudioPlayer />
+    </Router>  
+  </AudioPlayerProvider>
+  )
+}
