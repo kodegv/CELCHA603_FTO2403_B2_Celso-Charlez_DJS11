7cf239d8639a5720fbe48e4a7bfca06483754586
kodegv <celsocharlez1@gmail.com>
2024-08-22 15:08:44 +0200

    created file

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/main.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife'
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    terser()
+  ]
+};
